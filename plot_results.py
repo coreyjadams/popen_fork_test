10c2dd3cfcee7becdc93142f5d549b5d69eb3f3a
Corey Adams <corey.adams@anl.gov>
2020-05-05 14:59:34 +0000

    Updating scripts

--- plot_results.py
+++ plot_results.py
@@ -1,29 +1,83 @@
 import pickle
+import numpy
 from spawn_forks import job_spec
 
-
-outfile = "/projects/datascience/cadams/popen_fork_test/debugcache_1forks_150total_4096/output.pkl"
+folder  = "output/nid00021n_forks_100-max_concurrent_63-shell_True-worksize_64-reserve_host_True-import_mpi_False-use_affinity_True"
+outfile = f"/projects/datascience/cadams/popen_fork_test//{folder}/output.pkl"
 with open(outfile, 'rb') as _f:
-    finished_jobs = pickle.load(_f)
+    finished_jobs, CPU_USAGE, MEMORY = pickle.load(_f)
 
 
 starts = [ j.starttime for j in finished_jobs ]
 
 launch_start = min(starts)
 
-times  = [ j.runtime   for j in finished_jobs ]
-starts = [ s - launch_start for s in starts ]
-execs  = [ j.exec_time for j in finished_jobs ]
+times  = numpy.asarray([ j.runtime   for j in finished_jobs ] )
+starts = numpy.asarray([ s - launch_start for s in starts ]   )
+execs  = numpy.asarray([ j.exec_time for j in finished_jobs ] )
+ends   = numpy.asarray([ j.endtime - launch_start for j in finished_jobs])
+
+# Calculate and plot throughput:
+
+throughput_marks = numpy.arange(min(starts), max(ends), ( max(ends) - min(starts) ) / 50 )
 
 
-from matplotlib import pyplot as plt 
+n_completed_throughput = [ numpy.sum(ends < t) for t in throughput_marks]
+
+snapshot_times    = MEMORY.keys()
+memory_percentage = [ MEMORY[key].percent for key in snapshot_times]
+snapshot_times    = [t - launch_start for t in snapshot_times]
 
 
 
+from matplotlib import pyplot as plt
+# Create one big figure:
+fig = plt.figure(1, figsize=(30,16))
+
+
+# Plot the job completion times:
+plt.subplot(221)
+
+# Forking time as a function of start time:
 plt.scatter(starts, times, label="With Forks")
 plt.scatter(starts, execs, label="Just Application")
 plt.xlabel("Start time")
 plt.ylabel("Run time")
 plt.legend()
 plt.grid(True)
+plt.title("Job Runtime")
+
+# Throughput:
+plt.subplot(222)
+plt.plot(throughput_marks, n_completed_throughput, label="Completed jobs")
+plt.legend()
+ax2 = plt.gca().twinx()
+ax2.plot(snapshot_times, memory_percentage, color='red', label="Memory Usage")
+plt.ylim([0,100])
+plt.grid()
+plt.legend()
+plt.title("Throughput")
+
+# Plot the average fork times based on CPU
+
+cpus = numpy.asarray([ j.used_cpu for j in finished_jobs ])
+
+
+unique_cpus     = numpy.unique(cpus)
+masks = [ cpus == c for c in unique_cpus ]
+average_per_cpu = [ numpy.mean(times[m]) for m in masks ]
+rms_per_cpu     = [ numpy.std(times[m]) for m in masks ]
+
+locations = numpy.arange(len(unique_cpus))
+
+plt.subplot(212)
+plt.grid(zorder=0)
+plt.bar(locations, average_per_cpu, yerr=rms_per_cpu, label="With Forks")
+plt.xlabel("CPU ID")
+plt.ylabel("Average Run time")
+plt.xticks(ticks=locations, labels=unique_cpus)
+plt.legend()
+
+
+# Show everything
 plt.show()
--- spawn_forks.py
+++ spawn_forks.py
@@ -4,30 +4,92 @@ import pathlib
 import uuid
 import time
 import pickle
+import socket
+import argparse
+import json
+
+from signal import signal, SIGINT
+from sys import exit
 
 from subprocess import Popen, STDOUT, TimeoutExpired
 
 
+parser = argparse.ArgumentParser(
+    description     = 'Spawn forks for testing scaling',
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 
-#################################################
-#GLOBAL CONFIGURATION VARIABLES
-N_FORKS         = 150
-MAX_CONCURRENT  = 1
-N_CPUS          = 64
-SHELL           = True
-WORKSIZE        = 4096
-RESERVE_HOST    = False
-DEFAULT_AFF     = []
-TOP_PATH        = pathlib.Path(f"/projects/datascience/cadams/popen_fork_test/debugcache_{MAX_CONCURRENT}forks_{N_FORKS}total_{WORKSIZE}/")
-#################################################
+parser.add_argument('--n-forks',
+    type    = int,
+    default = 256,
+    help    = 'Total number of forks to spawn')
+
+parser.add_argument('--max-concurrent',
+    type    = int,
+    default = 4,
+    help    = 'Maximum number of running forks to use.')
+
+parser.add_argument('--shell',
+    type    = bool,
+    default = True,
+    help    = 'SHELL argument to subprocess')
+
+parser.add_argument('--worksize',
+    type    = int,
+    default = 4096,
+    help    = 'Size of problem to use in add_array')
+
+parser.add_argument('--reserve-host',
+    type    = bool,
+    default = False,
+    help    = 'If true, does not allow anything but the main loop run on CPU 0.')
+
+parser.add_argument('--output-directory',
+    type    = str,
+    default = os.getcwd(),
+    help    = 'Top directory to store data.')
+
+parser.add_argument('--output-file',
+    type    = str,
+    default = 'output.pkl',
+    help    = 'Name of file to dump statistics into.')
+
+parser.add_argument('--import-mpi',
+    type    = bool,
+    default = False,
+    help    = 'Import MPI and initialize, even though it isn\'t used.')
+
+parser.add_argument('--use-affinity',
+    type    = bool,
+    default = True,
+    help    = 'Use psutil to set affinity of forked jobs to an open CPU.')
 
 
+args = parser.parse_args()
+
+# Create the output folder name:
+# output_name = socket.gethostname() +f"_{args.max_concurrent}forks_{args.n_forks}total_{args.worksize}"
+s_list = []
+for arg in vars(args):
+    if "output" not in arg:
+        s_list.append(f"{arg}_{getattr(args, arg)}")
+
+output_name = socket.gethostname() + "-".join(s_list)
+
+
+TOP_PATH    = pathlib.Path(args.output_directory)
+TOP_PATH.mkdir(exist_ok=True)
+TOP_PATH = TOP_PATH / output_name
+
+
+N_CPUS   = psutil.cpu_count(logical=False)
+DEFAULT_AFF = []
+
+CPU_USAGE = {}
+MEMORY    = {}
 
 
 
 class job_spec(object):
-
-
 
     # Using state -1 = Nothing, 0 = running, 1 = done
 
@@ -51,7 +113,7 @@ def prepare_directory(TOP_PATH):
     return this_path
 
 
-def spawn_process(available_cpus, workdir, size=WORKSIZE):
+def spawn_process(available_cpus, workdir, size=args.worksize):
 
 
     job = job_spec()
@@ -60,7 +122,7 @@ def spawn_process(available_cpus, workdir, size=WORKSIZE):
     job.used_cpu = available_cpus.pop()
 
     job.workdir = workdir
-    
+
     # Set the outfile
     job.outfile = open(workdir / f"array_add_{size}.out", 'wb')
 
@@ -69,25 +131,32 @@ def spawn_process(available_cpus, workdir, size=WORKSIZE):
     _p = psutil.Process()
 
     # Set the affinity:
-    _p.cpu_affinity([job.used_cpu,])
+    if args.use_affinity:
+        _p.cpu_affinity([job.used_cpu,])
 
-    args = [f'python /projects/datascience/cadams/popen_fork_test/array_add.py {size}']
+    job_args = [f'python /projects/datascience/cadams/popen_fork_test/array_add.py {size}']
 
 
     # Spawn a process:
     job.starttime = time.time()
 
-    job.proc = Popen(args, stdout=job.outfile, stderr=STDOUT,
-                     cwd=workdir, env=os.environ, shell=SHELL,)
+    job.proc = Popen(job_args, stdout=job.outfile, stderr=STDOUT,
+                     cwd=workdir, env=os.environ, shell=args.shell,)
 
     job.state = 0
 
     # Restore the affinity to the default
-    _p.cpu_affinity(DEFAULT_AFF)
+    if args.use_affinity:
+        _p.cpu_affinity(DEFAULT_AFF)
 
     return available_cpus, job
 
 def check_active_jobs(active_jobs, available_cpus, timeout=1):
+
+    # The first thing done when checking jobs is to snapshot the CPU_USAGE
+    t = time.time()
+    CPU_USAGE[t] = psutil.cpu_times_percent(percpu=True)
+    MEMORY[t]    = psutil.virtual_memory()
 
     local_finished_jobs = []
     for i, job in enumerate(active_jobs):
@@ -125,34 +194,55 @@ def check_active_jobs(active_jobs, available_cpus, timeout=1):
     return active_jobs, local_finished_jobs
 
 
+def dump_args(args):
+
+    dump_location = TOP_PATH / "commandline_args.txt"
+    with open(dump_location, 'w') as f:
+        json.dump(args.__dict__, f, indent=2)
+
+
+
 def main():
 
-    if len(sys.argv) > 1:
-        output_file = sys.argv[-1]
-    else:
-        output_file = TOP_PATH / "output.pkl"
+
+    if args.import_mpi:
+        from mpi4py import MPI
+
+
+    ##########################################################
+    # Make this global to catch interrupts
+    global output_file
+    output_file = TOP_PATH / args.output_file
+    global finished_jobs
+    finished_jobs = []
+    ##########################################################
+    active_jobs = []
+
 
     # Make sure the top directory is available:
     TOP_PATH.mkdir(exist_ok=True)
 
+    # Persist the argument:
+    dump_args(args)
+
     # create a list of available CPUs, starting with all of them:
+    # available_cpus = [4*i for i in range(64)]
     available_cpus = list(range(N_CPUS))
 
-    if RESERVE_HOST:
+    if args.reserve_host:
+        # Take the main cpu off the available list
         DEFAULT_AFF.append(available_cpus.pop(0))
 
-    active_jobs = []
-    finished_jobs = []
 
     launch_start = time.time()
-
-    while len(finished_jobs) < N_FORKS:
-        # Loop until we've completed N_FORKS.
+    previous_time = launch_start
+    while len(finished_jobs) < args.n_forks:
+        # Loop until we've completed args.n_forks.
 
         # Do two things: launch jobs to start processing, and check for finished jobs
 
         # Make sure enough jobs are running:
-        while len(active_jobs) < MAX_CONCURRENT:
+        while len(active_jobs) < args.max_concurrent:
             workdir = prepare_directory(TOP_PATH)
             available_cpus, job = spawn_process(available_cpus, workdir)
             active_jobs.append(job)
@@ -161,13 +251,26 @@ def main():
         active_jobs, these_finished_jobs = check_active_jobs(active_jobs, available_cpus)
 
         finished_jobs += these_finished_jobs
-        print(f"Finished {len(finished_jobs)} jobs.")
+        snapshot_time = time.time()
+        throughput = len(these_finished_jobs) / (snapshot_time - previous_time)
+        print(f"Finished {len(finished_jobs)} jobs ({throughput:.2f} jobs/second).")
+        previous_time = snapshot_time
 
+    snapshot_finished_jobs()
+
+def snapshot_finished_jobs():
     # save the job list into a file:
     with open(output_file, 'wb') as _f:
-        pickle.dump(finished_jobs, _f)
+        pickle.dump([finished_jobs,CPU_USAGE, MEMORY] , _f)
 
+
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    snapshot_finished_jobs()
+    exit(0)
 
 
 if __name__ == "__main__":
+    signal(SIGINT, handler)
     main()
